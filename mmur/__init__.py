9039d278e3137f92825340666eced8f124dc1983
Ralph Urlus <rurlus.dev@gmail.com>
2021-12-12 23:17:03 +0100

    ENH: Add Stan functionality and models from MMU

--- mmur/__init__.py
+++ mmur/__init__.py
@@ -1,2 +1,13 @@
 from mmur.generators.logistic_generator import LogisticGenerator
 from mmur.generators.model_generator import ModelGenerator
+
+from mmur.models.beta_binomial import BetaBinomialConfusionMatrix
+from mmur.models.dirichlet_multinomial import DirichletMultinomialConfusionMatrix
+from mmur.models.dirichlet_multinomial import DirichletMultinomialMultiConfusionMatrix
+
+__all__ = [
+    'BetaBinomialConfusionMatrix',
+    'DirichletMultinomialConfusionMatrix',
+    'DirichletMultinomialMultiConfusionMatrix',
+    'LogisticGenerator',
+]
