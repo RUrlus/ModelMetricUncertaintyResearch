52ad51e0c58a2b633dad8661a167ee72b07185d5
Ralph Urlus <rurlus.dev@gmail.com>
2022-04-28 15:40:44 +0200

    CHG: Remove lep as it has become part of MMU

--- mmur/models/__init__.py
+++ mmur/models/__init__.py
@@ -1,12 +1,9 @@
 from mmur.models.beta_binomial import BetaBinomialConfusionMatrix
 from mmur.models.dirichlet_multinomial import DirichletMultinomialConfusionMatrix
 from mmur.models.dirichlet_multinomial import DirichletMultinomialMultiConfusionMatrix
-from mmur.models.lep import pr_uni_err_prop
-
 
 __all__ = [
     'BetaBinomialConfusionMatrix',
     'DirichletMultinomialConfusionMatrix',
     'DirichletMultinomialMultiConfusionMatrix',
-    'pr_uni_err_prop',
 ]
