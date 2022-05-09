0d819ef05bc5439f1764014e338fefbbb092b99d
coenvdm <coenvandermeijs@live.nl>
2022-05-09 15:41:07 +0200

    PY: Coen -- include multi cm implementation bootstrap

--- mmur/methods/single_run.py
+++ mmur/methods/single_run.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
 
-from mmur.simulators import LAlgorithm
+# from mmur.simulators import LAlgorithm
 
 
 def bstrp_cm(cm, n_draws=1, random_state=None):
@@ -13,7 +13,7 @@ def bstrp_cm(cm, n_draws=1, random_state=None):
 
     Parameters
     ----------
-    cm : np.ndarray of shape (4,)
+    cm : np.ndarray of shape (4,) or (N,4)
         Confusion matrix
     n_draws : int, optional
         number of bootstrap samples, by default 1
@@ -26,9 +26,28 @@ def bstrp_cm(cm, n_draws=1, random_state=None):
         Estimated distribution of confusion matrices
     """
     rng = np.random.default_rng(random_state)
-    n = cm.sum()
-    p = cm/n
-    return rng.multinomial(n, p, size=n_draws)
+
+    # Given a single confusion matrix
+    if len(cm.shape) == 1:
+        n = cm.sum()
+        p = cm/n
+        return rng.multinomial(n, p, size=n_draws)
+
+    # For multiple confusion matrices
+    elif len(cm.shape) == 2:
+        n = cm.sum(axis=1)
+        p = cm/n.reshape(-1,1)
+        return rng.multinomial(n,p,size = (n_draws,cm.shape[0]))
+
+    else:
+        raise ValueError('Parameter ``cm`` is of wrong shape')
+
+
+def bstrp_multi_cm(cms,n_draws=1,random_state=None):
+    rng = np.random.default_rng(random_state)
+    n = cms.sum(axis=0)
+    p = cms/n
+    return rng.multinomial(n,p,size = (n_draws,len(cms)))
 
 
 def kfold_cm(X, y, model_class, model_kwargs={}, n_splits=5, random_state=None):
@@ -78,7 +97,7 @@ def pred_iv(values, q_range=0.95):
 
 
 if __name__ == '__main__':
-    model = LogisticRegression
-    X = np.random.uniform(size=(1000, 2))
-    y = np.random.choice([0, 1], size=1000)
-    kfold_cm(X, y, model)
+    rng = np.random.default_rng(1)
+    cms = rng.multinomial(100,pvals=[0.2,0.3,0.2,0.3],size= 5)
+    cm_bs = bstrp_cm(cms,n_draws=3,random_state = rng)
+    cm_bs
