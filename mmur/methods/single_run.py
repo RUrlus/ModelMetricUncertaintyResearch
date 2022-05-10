546c6f3ed2cdf935b4e498aa161c312c81d49fde
coenvdm <coenvandermeijs@live.nl>
2022-05-10 11:18:56 +0200

    PY/NTB: updated bootstrap and kfold functions

--- mmur/methods/single_run.py
+++ mmur/methods/single_run.py
@@ -4,22 +4,20 @@ import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
+# import mmu
 
-# from mmur.simulators import LAlgorithm
-
-
-def bstrp_cm(cm, n_draws=1, random_state=None):
+def bstrp_cm(cm, n_draws=1000, random_state=None):
     """Bootstrap a confusion matrix, equivalent to bootstrapping the test set and then computing the confusion matrices (given a deterministic predictor). To obtain a collection of confusion matrices approximating the confusion matrix' distribution.
 
     Parameters
     ----------
     cm : np.ndarray of shape (4,) or (N,4)
-        Confusion matrix
+        Confusion matrix or array of `N` confusion matrices
     n_draws : int, optional
-        number of bootstrap samples, by default 1
+        Number of bootstrap samples, by default 1000
     random_state : int, optional
         Set to a value for reproducible results, by default None
-
++-------------  
     Returns
     -------
     np.ndarray of shape (n_draws, 4)
@@ -37,18 +35,12 @@ def bstrp_cm(cm, n_draws=1, random_state=None):
     elif len(cm.shape) == 2:
         n = cm.sum(axis=1)
         p = cm/n.reshape(-1,1)
-        return rng.multinomial(n,p,size = (n_draws,cm.shape[0]))
+        cm_bs = np.swapaxes(rng.multinomial(n,p,size = (n_draws,cm.shape[0])),0,1)
+        # Swap axes, such that the first axis corresponds to the number of input confusion matrices
+        return cm_bs
 
     else:
-        raise ValueError('Parameter ``cm`` is of wrong shape')
-
-
-def bstrp_multi_cm(cms,n_draws=1,random_state=None):
-    rng = np.random.default_rng(random_state)
-    n = cms.sum(axis=0)
-    p = cms/n
-    return rng.multinomial(n,p,size = (n_draws,len(cms)))
-
+        raise ValueError('Parameter ``cm`` is of wrong shape, should be of shape (4,) or (N,4)')
 
 def kfold_cm(X, y, model_class, model_kwargs={}, n_splits=5, random_state=None):
     """Performs k-fold cross validation and computes the confusion matrix for each split.
@@ -88,8 +80,31 @@ def kfold_cm(X, y, model_class, model_kwargs={}, n_splits=5, random_state=None):
 
     return cms
 
+#TODO: K-fold cross val in parallel
 
-# TODO: Multinomial CM with Dirichlet prior, need MMU for this
+def kfold_cv_cm(X, y, model_class, model_kwargs={}, n_splits=5, random_state=None):
+    n_obs = len(y)
+    if n_obs % n_splits != 0:
+        raise ValueError('The number of observations is not divisible by the number of splits')
+
+    rng = np.random.default_rng(random_state)
+    fold_seed = rng.integers(low=0, high=np.iinfo(
+        np.uint32).max)  # TODO: seeding
+    kf = KFold(n_splits, shuffle=True, random_state=fold_seed)
+
+    n_test_obs = len(y)
+    y_hat = np.zeros((n_splits,n_test_obs),dtype=int)
+    y_true = np.zeros((n_splits,n_test_obs),dtype=int)
+    for i, (train_index, test_index) in enumerate(kf.split(X)):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        clf = model_class(**model_kwargs).fit(X_train, y_train)
+
+        y_hat[i,:] = clf.predict(X_test)
+        y_true[i,:] = y_test
+
+    return mmu.confusion_matrices(y_true,y_hat,obs_axis=1)
 
 
 def pred_iv(values, q_range=0.95):
@@ -98,6 +113,7 @@ def pred_iv(values, q_range=0.95):
 
 if __name__ == '__main__':
     rng = np.random.default_rng(1)
-    cms = rng.multinomial(100,pvals=[0.2,0.3,0.2,0.3],size= 5)
-    cm_bs = bstrp_cm(cms,n_draws=3,random_state = rng)
-    cm_bs
+    X = rng.normal(size = (100,2))
+    y = rng.binomial(1,0.4,size=(100))
+    cv_cm = kfold_cv_cm(X, y, LogisticRegression, model_kwargs={'penalty':'none'}, n_splits=5, random_state=None)
+    print(cv_cm)
